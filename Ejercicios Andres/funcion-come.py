4b93351fc7f8761d6d125ca628f61f53f5867a79
ScarletteBel <83143055+ScarletteBel@users.noreply.github.com>
2021-09-22 17:00:18 -0500

    Added new folder

---
+++ Ejercicios Andres/funcion-come.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+def para_comersela(nombre):
+    print (nombre + " se la come")
+    
+
+para_comersela("Andres")
+
+
+
+def come(name):
+    return name + " se la come"
+
+print(come('Andrés'))
