0b6adef6293077d8c028537a77ccd04b732c4a0b
ScarletteBel <83143055+ScarletteBel@users.noreply.github.com>
2021-09-24 17:59:31 -0500

    added second function

---
+++ Practice excercises/function_ till_100.py
@@ -0,0 +1,16 @@
+def count_till_number(number):
+    n = 0
+    while n <= number:
+        print (n)
+        n += 1
+        
+        
+count_till_number(100)
+
+
+for n in range (101):
+    print (n)
+        
+    
+       
+    
